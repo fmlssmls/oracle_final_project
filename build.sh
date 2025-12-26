3fe77d64a9653e5fc441cc451ab32349d17b2e8a
fmlssmls <wlsud0601@naver.com>
2025-12-26 14:24:31 +0900

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-wget https://download.oracle.com/otn_software/linux/instantclient/2340000/instantclient-basic-linux.x64-23.4.0.24.05.zip
-unzip instantclient-basic-linux.x64-23.4.0.24.05.zip -d /opt/
-rm instantclient-basic-linux.x64-23.4.0.24.05.zip
-
-pip install -r requirements.txt
