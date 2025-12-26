aa4cb7d3b45a3c416f22c70aed10f488b3e5a8d5
fmlssmls <wlsud0601@naver.com>
2025-12-26 14:03:31 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Oracle Instant Client 설치
 wget https://download.oracle.com/otn_software/linux/instantclient/2340000/instantclient-basic-linux.x64-23.4.0.24.05.zip
 unzip instantclient-basic-linux.x64-23.4.0.24.05.zip -d /opt/
 rm instantclient-basic-linux.x64-23.4.0.24.05.zip
 
-# Python 패키지 설치
 pip install -r requirements.txt
